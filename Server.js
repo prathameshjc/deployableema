a34aea6e6ab521ead02f5b756ed3e28f22fce1ad
Prathamesh Jadhav <prathameshjc@gmail.com>
2020-08-09 03:04:33 +0530

    stackover flow code

--- Server.js
+++ Server.js
@@ -45,8 +45,8 @@ server.get('/employees',demotable)
 
 if(process.env.NODE_ENV==='production')
 {
-  server.get('/', restify.plugins.serveStatic({
-    directory: (__dirname,"client/build"),
+  server.get(/.*/, restify.plugins.serveStatic({
+    directory: __dirname,
     default: 'index.html'
    }));
 
