93b2c27d815100f59b71017dcd6e9c4917e001ef
Prathamesh Jadhav <prathameshjc@gmail.com>
2020-08-08 23:46:32 +0530

    restify server configured as per instructions

--- Server.js
+++ Server.js
@@ -42,13 +42,13 @@ server.use(restify.plugins.bodyParser());
 server.get('/login',logIn)
 server.get('/employees',demotable)
 
-if(process.env.NODE_ENV==='production')
-{
-server.use(restify.serveStatic('client/build'))
-server.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'client','build','index.html'))
-})
-}
+// if(process.env.NODE_ENV==='production')
+// {
+// server.use(restify.serveStatic('client/build'))
+// server.get('*',(req,res)=>{
+//   res.sendFile(path.join(__dirname,'client','build','index.html'))
+// })
+// }
 // server.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "client/build/index.html"));
 // });
