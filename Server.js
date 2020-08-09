f17baac5e186c13ed3eac4d1f69d46b5fd47b42c
Prathamesh Jadhav <prathameshjc@gmail.com>
2020-08-09 20:12:05 +0530

    demo

--- Server.js
+++ Server.js
@@ -1,9 +1,11 @@
+
 var restify=require('restify')
-const { logIn,userCreation, demotable } = require('./Function');
+const { logIn,userCreation, demotable } = require('./routes/Function');
 const corsMiddleware = require('restify-cors-middleware2');
 const { data } = require('jquery');
 const PORT=process.env.PORT || 8080;
 var path=require('path')
+var serveStatic = require('serve-static-restify')
 var server=restify.createServer() //server created
 
 server.use(
@@ -26,13 +28,47 @@ const cors = corsMiddleware({
 
 server.pre(cors.preflight)
 server.use(cors.actual)
+//setting
+if (process.env.NODE_ENV === "production") {
+  //server static content
+  //npm run build
+  server.use(restify.static(path.join(__dirname, "client/build")));
+}
 
+console.log(__dirname);
+console.log(path.join(__dirname, "client/build"));
 //get data from login form
-
-server.post('/note', userCreation);  
+// server.get("/",(req,res)=>{ restify.plugins.serveStatic({
+//       directory: __dirname,
+//       default: 'index.html',
+//       })
+//       res.sendFile(path.join(__dirname,'client','build','index.html'))
+//     });
+server.post('/', userCreation);  
 server.use(restify.plugins.bodyParser());
 server.get('/login',logIn)
 server.get('/employees',demotable)
+
+
+// if(process.env.NODE_ENV==='production')
+// {
+//   server.get("/",(req,res)=>{ restify.plugins.serveStatic({
+//     directory: __dirname,
+//     default: 'index.html',
+    
+//    })
+   
+//   });
+
+// // server.use(restify.serveStatic('client/build'))
+// // server.get('*',(req,res)=>{
+// //   res.sendFile(path.join(__dirname,'client','build','index.html'))
+// // })
+// }
+// server.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
 server.listen(PORT, function(){
     console.log("server started...")
+
 })
