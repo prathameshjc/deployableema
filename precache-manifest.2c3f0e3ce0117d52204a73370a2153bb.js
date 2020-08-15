3002972ae1f013e96ddd8bcc94f64dfafebbcd2b
Prathamesh Jadhav <prathameshjc@gmail.com>
2020-08-16 00:00:47 +0530

    Updates

---
+++ precache-manifest.2c3f0e3ce0117d52204a73370a2153bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0464f30722d95cc7cf68b4674bf187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4ca872e04a64e4d6a17",
+    "url": "/static/css/main.305b25b3.chunk.css"
+  },
+  {
+    "revision": "5f1554e034f75e5f321b",
+    "url": "/static/js/2.c570e064.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c570e064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ca872e04a64e4d6a17",
+    "url": "/static/js/main.cbcc4cc1.chunk.js"
+  },
+  {
+    "revision": "ea4d445f79ff10ee18a9",
+    "url": "/static/js/runtime-main.b0f6b724.js"
+  }
+]);
